f597ca86bc9cfe72ca9ce5a7868a76ac44d6b71d
Raphael <82618047+RandyePX@users.noreply.github.com>
2022-05-18 16:59:39 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Dados'),
+          backgroundColor: Colors.blue,
+        ),
+        body: _novaPaginaDados(),
+      ),
+    ),
+  );
+}
+
+class _novaPaginaDados extends StatefulWidget {
+  const _novaPaginaDados({Key? key}) : super(key: key);
+
+  @override
+  State<_novaPaginaDados> createState() => new _novaPaginaDadosState();
+}
+
+class _novaPaginaDadosState extends State<_novaPaginaDados> {
+  int valorTirado1 = 2;
+  int valorTirado2 = 3;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  valorTirado1 = Random().nextInt(6) + 1;
+                }
+                );
+              },
+              child: Image.asset('images/dado$valorTirado1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  valorTirado2 = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dado$valorTirado2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
